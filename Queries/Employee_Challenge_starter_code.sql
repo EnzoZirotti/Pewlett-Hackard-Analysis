f1ab8368e975568bd9c2a921666f04e9f5154aab
Enzo <Zirotti32@gmail.com>
2022-02-04 20:43:25 -0700

    Deliverable 2 done

--- Queries/Employee_Challenge_starter_code.sql
+++ Queries/Employee_Challenge_starter_code.sql
@@ -1,15 +1,67 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-FROM 
-SELECT d.dept_name,
---      dm.emp_no,
---      dm.from_date,
---      dm.to_date
--- FROM departments as d
+-- SELECT e.emp_no,
+-- e.first_name,
+-- e.last_name,
+-- ti.title,
+-- ti.from_date,
+-- ti.to_date
+-- -- INTO retirement_titles
+-- FROM employees as e
+-- JOIN titles as ti
+-- ON e.emp_no = ti.emp_no
+-- WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- ORDER BY emp_no, emp_no DESC;
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+-- Use Dictinct with Orderby to remove duplicate rows
+
+-- SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+-- rt.first_name,
+-- rt.last_name,
+-- rt.title
+-- -- INTO unique_titles
+-- FROM retirement_titles as rt
+-- WHERE to_date = '9999-01-01' 
+-- ORDER BY emp_no, to_date DESC;
+
+-- SELECT COUNT(ut.emp_no), ut.title 
+-- INTO retiring_titles 
+-- FROM unique_titles as ut 
+-- GROUP BY ut.title 
+-- ORDER BY COUNT(ut.title) DESC; 
+
+
+-- SELECT DISTINCT ON (e.emp_no) e.emp_no,
+-- e.first_name,
+-- e.last_name,
+-- e.birth_date,
+-- de.from_date,
+-- de.to_date,
+-- ti.title
+-- -- -- INTO retirement_titles
+-- FROM employees as e
+-- JOIN dept_emp as de
+-- ON e.emp_no = de.emp_no
+-- JOIN titles as ti
+-- ON e.emp_no = ti.emp_no
+-- WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+-- AND (de.to_date = '9999-01-01')
+-- ORDER BY emp_no, emp_no ASC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
