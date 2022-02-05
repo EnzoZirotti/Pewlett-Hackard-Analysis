da1d03339ad9a43784b6637475b63a3236a4a28e
Enzo <Zirotti32@gmail.com>
2022-02-04 18:48:53 -0700

    Deliverable Push 1

--- Queries/Employee_Challenge_starter_code.sql
+++ Queries/Employee_Challenge_starter_code.sql
@@ -1,30 +1,15 @@
--- SELECT e.emp_no,
--- e.first_name,
--- e.last_name,
--- ti.title,
--- ti.from_date,
--- ti.to_date
--- -- INTO retirement_titles
--- FROM employees as e
--- JOIN titles as ti
--- ON e.emp_no = ti.emp_no
--- WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
--- ORDER BY emp_no, emp_no DESC;
-
 -- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+FROM 
+SELECT d.dept_name,
+--      dm.emp_no,
+--      dm.from_date,
+--      dm.to_date
+-- FROM departments as d
 
--- SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
--- rt.first_name,
--- rt.last_name,
--- rt.title
--- -- INTO unique_titles
--- FROM retirement_titles as rt
--- WHERE to_date = '9999-01-01' 
--- ORDER BY emp_no, to_date DESC;
-
--- SELECT COUNT(ut.emp_no), ut.title 
--- INTO retiring_titles 
--- FROM unique_titles as ut 
--- GROUP BY ut.title 
--- ORDER BY COUNT(ut.title) DESC; 
-
+INTO nameyourtable
+FROM _______
+WHERE _______
+ORDER BY _____, _____ DESC;
